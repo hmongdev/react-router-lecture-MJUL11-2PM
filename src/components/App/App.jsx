d052e8a62affd1d54823f26421574b980359c7a3
Avery Yang <averyyang2674@gmail.com>
2022-07-11 14:53:46 -0500

    create Router, Links, and Paths

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+//1. import HashRouter, Route, Link from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import Animals from '../Animals/Animals.jsx';
@@ -7,11 +9,34 @@ import Home from '../Home/Home.jsx';
 
 export default function App() {
     return (
-        <div className="App">
-            <h1>SPAs!</h1>
-            <Home />
-            <Plants />
-            <Animals />
-        </div>
+        //2. Create a <Router> element <Router></Router>
+        //3. Create a <Route path="/"> for each component
+        //4. Include exact in <Route path="/">
+        //5. Import Link, and Create Links for each component: <li></li>
+        <Router>
+            <div className="App">
+                <h1>SPAs!</h1>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/plants">Plants</Link>
+                    </li>
+                    <li>
+                        <Link to="/animals">Animals</Link>
+                    </li>
+                </ul>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/plants">
+                    <Plants />
+                </Route>
+                <Route path="/animals">
+                    <Animals />
+                </Route>
+            </div>
+        </Router>
     );
 }
